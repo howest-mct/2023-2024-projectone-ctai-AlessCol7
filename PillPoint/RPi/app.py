aef244b1d7e462fa11c6c47eb84f0457a92d8e22
AlessCol7 <alessia.columban@student.howest.be>
2024-05-27 14:56:48 +0200

    commit

--- PillPoint/RPi/app.py
+++ PillPoint/RPi/app.py
@@ -1,9 +1,26 @@
-# RPi/app.py
-
 import cv2
 import time
+import queue
+import threading
 from Buzzer import BuzzerController
 from AI.app import detect_objects, is_pill_taken, log_pill_taken_event
+from bluetooth_uart_server.bluetooth_uart_server import ble_gatt_uart_loop
+
+def ble_gatt_uart_loop(rx_q, tx_q, device_name):
+    # BLE GATT UART loop code here
+    i = 0
+    rx_q = queue.Queue()
+    tx_q = queue.Queue()
+    device_name = "AlessCol-pi-gatt-uart" # replace with your own (unique) device name
+    threading.Thread(target=ble_gatt_uart_loop, args=(rx_q, tx_q, device_name), daemon=True).start()
+    while True:
+        try:
+            incoming = rx_q.get(timeout=1) # Wait for up to 1 second 
+            if incoming:
+                print("In main loop: {}".format(incoming))
+        except Exception as e:
+            pass # nothing in Q 
+    
 
 def capture_image():
     vid = cv2.VideoCapture(0)
@@ -35,5 +52,10 @@ def main():
             print(f"An error occurred: {e}")
             time.sleep(60)  # Wait a minute before trying again
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    i = 0
+    rx_q = queue.Queue()
+    tx_q = queue.Queue()
+    device_name = "AlessCol-pi-gatt-uart"
+    threading.Thread(target=ble_gatt_uart_loop, args=(rx_q, tx_q, device_name), daemon=True).start()
     main()
